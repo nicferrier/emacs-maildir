5a5aeedbb87fe62ac5d4c73a4332475ebfcf4716
Nic Ferier <nic@ferrier.me.uk>
2012-12-09 00:06:58 +0000

    bump the version

--- maildir.el
+++ maildir.el
@@ -5,7 +5,7 @@
 ;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Maintainer: Nic Ferrier <nferrier@ferrier.me.uk>
 ;; Keywords: mail, files
-;; Version: 0.0.3
+;; Version: 0.0.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
